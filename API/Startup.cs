3a6234abec268995bdbc72dfe14874f56e48f887
Gregory Miola Da Silva <reptil9999@gmail.com>
2019-01-30 18:20:00 -0200

    Prototype of project, first commit.

---
+++ API/Startup.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+using System.Globalization;
+using System.IO;
+
+namespace API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.Configure<GzipCompressionProviderOptions>(options => options.Level = System.IO.Compression.CompressionLevel.Optimal);
+
+            services.AddResponseCompression();
+
+            //// Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "API ...",
+                    Description = "API de Exemplo"
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                string applicationPath = PlatformServices.Default.Application.ApplicationBasePath;
+                string applicationName = PlatformServices.Default.Application.ApplicationName;
+                string pathXmlDoc = Path.Combine(applicationPath, $"{applicationName}.xml");
+                c.IncludeXmlComments(pathXmlDoc);
+            });
+
+            RegisterServices(services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseResponseCompression();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+                c.RoutePrefix = "swagger/ui";
+            });
+
+            app.UseMvc();
+
+            var cultureInfo = new CultureInfo("pt-BR");
+
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            //IoCConfig.RegisterServices(services);
+        }
+    }
+}
---
+++ CrossCutting/IoC/IoCConfig.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CrossCutting.IoC
+{
+    /// <summary>
+    /// Classe responsável pela configuração da injeção de dependências do sistema 
+    /// </summary>
+    public class IoCConfig
+    {
+        /// <summary>
+        /// Método que registra todas as classes/interfaces que serão injetadas no sistema
+        /// </summary>
+        /// <param name="services">Serviço de configuração da injeção de dependência</param>
+        public static void RegisterServices(IServiceCollection services)
+        {
+            #region Services
+            //services.AddTransient<ISaleService, SaleService>();
+            //services.AddTransient<IHttpHandler, HttpClientHandler>();
+            #endregion Services
+
+            #region Repositories
+            //services.AddTransient<ISaleRepository, SaleRepository>();
+            #endregion Repositories
+
+            #region Contexts
+            //services.AddTransient<SalesContext>();
+            #endregion Contexts
+        }
+    }
+}
